03bea68f3434b562af2c0790dec6df99e92148c3
diamijaafar <diamijaafar@gmail.com>
2017-02-03 17:33:45 +0100

    Initial commit

---
+++ src/main/java/sonde/Services/DisparitionComposant.java
@@ -0,0 +1,7 @@
+package sonde.Services;
+
+/**
+ * Created by Jaafar on 03/02/2017.
+ */
+public interface DisparitionComposant {
+}
---
+++ src/main/java/sonde/Services/Notification.java
@@ -0,0 +1,7 @@
+package sonde;
+
+/**
+ * Created by Jaafar on 03/02/2017.
+ */
+public interface Notification {
+}
