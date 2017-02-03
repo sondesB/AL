2c7b258512f52e55c6b4ea229d6b316922e0c66a
Sebastien Gadois <sgadois@gmail.com>
2017-02-03 15:05:20 +0100

    Début implementation log

--- src/main/java/log/implementations/LogImpl.java
+++ src/main/java/log/implementations/LogImpl.java
@@ -1,9 +1,12 @@
 package log.implementations;
 
 import log.interfaces.ILog;
+import log.services.LogService;
 
 /**
  * Created by seb on 03/02/17.
  */
 public class LogImpl implements ILog {
+
+    private LogService logService = new LogService();
 }
