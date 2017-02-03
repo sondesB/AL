69a85bddcfb1eadffc1362be6d427a178d9afb4e
BlueSkyBalmung <vincent.heit@gmail.com>
2017-02-03 15:01:58 +0100

    interface V1

--- src/main/java/log/implementations/LogImpl.java
+++ src/main/java/log/implementations/LogImpl.java
@@ -6,4 +6,12 @@ import log.interfaces.ILog;
  * Created by seb on 03/02/17.
  */
 public class LogImpl implements ILog {
+
+    public void loggerDecision(){
+
+    }
+
+    public void loggerComposant(){
+
+    }
 }
--- src/main/java/log/interfaces/ILog.java
+++ src/main/java/log/interfaces/ILog.java
@@ -4,4 +4,7 @@ package log.interfaces;
  * Created by seb on 03/02/17.
  */
 public interface ILog {
+    public void loggerComposant();
+
+    public void loggerDecision();
 }
