7acbd0817b50240bb77f66a04866e3f5f9a28a64
fredericDuarte <frederic.duarte@master-developpement-logiciel.fr>
2017-02-03 16:42:29 +0100

    MAJ Commentaire

--- src/main/java/log/services/LogService.java
+++ src/main/java/log/services/LogService.java
@@ -10,15 +10,24 @@ import java.io.PrintWriter;
 public class LogService {
 
     File log;
-
+    /**
+     * Création d'un nouveau fichier 
+     * @param chemin du fichier
+     */
     public LogService(String path) {
         this.log = new File(path);
     }
 
+    /**
+     *  Création d'un nouveau fichier log par défauts
+     */
     public LogService() {
         this.log = new File("log");
     }
-
+    /**
+     * Ecriture 
+     * @param ligne
+     */
     public void ecrireLog(String logLine) {
         try {
             PrintWriter pw = new PrintWriter(this.log);
