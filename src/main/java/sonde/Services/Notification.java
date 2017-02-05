0dff251fd71d978c8040b33119630ef0cf7c7c0f
kacimimoghite <moghitkacimi@gmail.com>
2017-02-05 20:44:24 +0100

    Ajouter methode pour l'interface notification

--- src/main/java/sonde/Services/Notification.java
+++ src/main/java/sonde/Services/Notification.java
@@ -1,7 +1,23 @@
 package sonde.Services;
 
+import interfaceswcomp.OCComponent;
+
+import java.util.Set;
+
 /**
  * Created by Jaafar on 03/02/2017.
  */
 public interface Notification {
+
+    /**
+     * Communiquer les composants apparus
+     * @param composants les composants apparus
+     */
+    void createAgents(Set<OCComponent> composants);
+
+    /**
+     * Communiquer les composants disparus
+     * @param composantsDisparus les composants disparus
+     */
+    void notifyDisappearedComponents(Set<OCComponent> composantsDisparus);
 }
