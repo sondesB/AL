678784ff5824d9583a6eba6e62cd7734c40336e5
kacimimoghite <moghitkacimi@gmail.com>
2017-02-05 18:57:59 +0100

    Ajout des commentaires

--- src/main/java/sonde/Services/ILogComposant.java
+++ src/main/java/sonde/Services/ILogComposant.java
@@ -9,13 +9,13 @@ import java.util.Set;
  */
 public interface ILogComposant {
     /**
-     * Tracer le log des nouveaux Composants
+     * Tracer le log des nouveaux Composants via la méthode fournit par le composant Log
      * @param nouveauxComposants la liste des nouveaux composants
      */
     void getLogApparitionComposant(Set<OCComponent> nouveauxComposants);
 
     /**
-     * Tracer le log des composants disparus
+     * Tracer le log des composants disparus via la méthode fournit par le composant Log
      * @param composantsDisparus la liste des composants disparus
      */
     void getLogDispparitionComposant(Set<OCComponent> composantsDisparus);
