9dec6966f3ecb9031e8b8dd387598b35d5fd0032
kacimimoghite <moghitkacimi@gmail.com>
2017-02-05 18:50:03 +0100

    Implements interface Configuration

---
+++ src/main/java/sonde/Impl/ConfigurationImpl.java
@@ -0,0 +1,15 @@
+package sonde.Impl;
+
+import sonde.Services.Configuration;
+
+/**
+ * Created by Moghite on 05/02/2017.
+ */
+public class ConfigurationImpl implements Configuration {
+
+    @Override
+    public int getPeriodiciteSondage() {
+        //Retouner la valeur récupérée de l'IHM
+        return 0;
+    }
+}
--- src/main/java/sonde/Services/Configuration.java
+++ src/main/java/sonde/Services/Configuration.java
@@ -4,4 +4,9 @@ package sonde.Services;
  * Created by Jaafar on 03/02/2017.
  */
 public interface Configuration {
+    /**
+     *
+     * @return la perdiodicité de sondage fournit par l'utilisateur
+     */
+    int getPeriodiciteSondage();
 }
