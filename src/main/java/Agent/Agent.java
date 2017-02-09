f4e93b29931fd098b7ba1a99610bb6a8094ad4b2
Nicolas <nicolas2smet@hotmail.fr>
2017-02-09 17:37:29 +0100

    Définition de l'agent

--- src/main/java/Agent/Agent.java
+++ src/main/java/Agent/Agent.java
@@ -1,8 +1,24 @@
 package Agent;
 
+import java.util.ArrayList;
+
 /**
  * Created by Nicolas on 09/02/2017.
  */
 public abstract class Agent {
+    private List<AbstractPerception> perceptions = new ArrayList<AbstractPerception>(); // Liste des objets perception
+    private List<AbstractDecision> decisions = new ArrayList<AbstractDecision>(); // Liste des objets decision
+    private CycleDeVie cycle;
 
+    public abstract void run();
+
+    public abstract List<AbstractPerception> getPerceptions();
+
+    public abstract List<AbstractDecision> getDecisions();
+
+    public abstract void setPerceptions(List<AbstractPerception> perceptions);
+
+    public abstract void setDecisions(List<AbstractDecision> decisions);
+
+    public abstract void setCycleDeVie(CycleDeVie cycle);
 }
