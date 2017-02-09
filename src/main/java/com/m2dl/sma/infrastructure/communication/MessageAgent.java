4841a6d1e157bf27b973e35b0dc4fa2c6a552c7d
Arthur <bellal.arthur@gmail.com>
2017-02-09 16:41:31 +0100

    Ajout de l'expediteur au message

--- src/main/java/com/m2dl/sma/infrastructure/communication/MessageAgent.java
+++ src/main/java/com/m2dl/sma/infrastructure/communication/MessageAgent.java
@@ -1,5 +1,8 @@
 package com.m2dl.sma.infrastructure.communication;
 
+import com.m2dl.sma.infrastructure.agent.ReferenceAgent;
+
 public interface MessageAgent {
 
+    ReferenceAgent getExpediteur();
 }
