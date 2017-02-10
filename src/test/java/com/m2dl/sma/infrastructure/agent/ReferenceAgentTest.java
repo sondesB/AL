b069c157788b14698fa97482957ff43dcb663761
Thomas Bille <thomas.bille625@gmail.com>
2017-02-10 17:42:20 +0100

    Add missing tests

--- src/test/java/com/m2dl/sma/infrastructure/agent/ReferenceAgentTest.java
+++ src/test/java/com/m2dl/sma/infrastructure/agent/ReferenceAgentTest.java
@@ -2,6 +2,7 @@ package com.m2dl.sma.infrastructure.agent;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
@@ -15,6 +16,20 @@ public class ReferenceAgentTest {
         ReferenceAgent secondeReferenceAgent = new ReferenceAgent();
 
         assertThat(premiereReferenceAgent, not(equalTo(secondeReferenceAgent)));
+    }
+
+    @Test
+    public void devrais_retourner_false_si_null() throws Exception {
+        ReferenceAgent premiereReferenceAgent = new ReferenceAgent();
+
+        assertFalse(premiereReferenceAgent.equals(null));
+    }
+
+    @Test
+    public void devrais_retourner_false_si_autre_type() throws Exception {
+        ReferenceAgent premiereReferenceAgent = new ReferenceAgent();
+
+        assertFalse(premiereReferenceAgent.equals(new String()));
     }
 
     @Test
