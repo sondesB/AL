bc80530b538345fe6033597b6278c03f03fc2444
BICHON Jonathan <jo.bichon1@gmail.com>
2017-02-10 15:28:23 +0100

    Ajout classe StrategieEtatAEtat et StrategieClassique

---
+++ src/main/java/com/m2dl/sma/infrastructure/ordonnanceur/StrategieClassique.java
@@ -0,0 +1,20 @@
+package com.m2dl.sma.infrastructure.ordonnanceur;
+
+import com.m2dl.sma.infrastructure.EnumVitesse;
+
+public class StrategieClassique implements IStratOrdonnanceur {
+    @Override
+    public void ordonnancer() {
+
+    }
+
+    @Override
+    public void changerVitesse(EnumVitesse vitesse) {
+
+    }
+
+    @Override
+    public void arreterOrdonnancement() {
+
+    }
+}
