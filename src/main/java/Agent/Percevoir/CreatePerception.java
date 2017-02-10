f8fcde6964d1f7d953c0bab7097ddf7e9130140a
Girox64 <kevingiraud64@gmail.com>
2017-02-10 13:49:03 +0100

    Squelette Perception

---
+++ src/main/java/Agent/Percevoir/CreatePerception.java
@@ -0,0 +1,7 @@
+package Agent.Percevoir;
+
+/**
+ * Created by Kévin on 09/02/2017.
+ */
+public class CreatePerception implements ICreatePerception{
+}
---
+++ src/main/java/Agent/Percevoir/ICreatePerception.java
@@ -0,0 +1,7 @@
+package Agent.Percevoir;
+
+/**
+ * Created by Kévin on 09/02/2017.
+ */
+public interface ICreatePerception {
+}
