10cf5d788a0336dc1c821f2ec6bacaba6bec8f56
BICHON Jonathan <jo.bichon1@gmail.com>
2017-02-10 14:34:13 +0100

    Creation class StrategieClassique and StrategieEtatAEtat

---
+++ src/main/java/com/m2dl/sma/infrastructure/ordonnanceur/StrategieClassique.java
@@ -0,0 +1,23 @@
+package com.m2dl.sma.infrastructure.ordonnanceur;
+
+import com.m2dl.sma.infrastructure.agent.Agent;
+
+/**
+ * Created by BICHON Jonathan on 10/02/2017.
+ */
+public class StrategieClassique implements IStratOrdonnanceur {
+    @Override
+    public void ordonnancer() {
+
+    }
+
+    @Override
+    public void updateAgent(Agent agent) {
+
+    }
+
+    @Override
+    public void updateSuppr(Agent agent) {
+
+    }
+}
