6168d0f14acd8e1bdf3657ca13840d768a8ecdc8
Rottana <rottanaly@ymail.com>
2017-02-10 14:10:55 +0100

    Periodicite Sondage

--- src/main/java/sonde/Composants/PeriodiciterSondage.java
+++ src/main/java/sonde/Composants/PeriodiciterSondage.java
@@ -1,7 +1,14 @@
 package sonde.Composants;
 
+import javax.swing.*;
+
 /**
  * Created by Jaafar on 03/02/2017.
  */
-public class PeriodiciterSondage {
+public class PeriodiciterSondage extends JFrame {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel labelPeriodicite;
+
 }
