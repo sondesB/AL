39521dbd1471babb0bc9ed2b5cdb2e9a6ec0c10e
bouletator <clement.romanet@gmail.com>
2017-02-10 17:08:18 +0100

    Ordonnanceurs sans changement de vitesse

--- src/main/java/com/m2dl/sma/infrastructure/ordonnanceur/IStratOrdonnanceur.java
+++ src/main/java/com/m2dl/sma/infrastructure/ordonnanceur/IStratOrdonnanceur.java
@@ -2,11 +2,14 @@ package com.m2dl.sma.infrastructure.ordonnanceur;
 
 import com.m2dl.sma.infrastructure.EnumVitesse;
 import com.m2dl.sma.infrastructure.agent.Agent;
+import com.m2dl.sma.infrastructure.annuaire.AgentListener;
 
-public interface IStratOrdonnanceur {
+import java.util.List;
+
+public interface IStratOrdonnanceur extends AgentListener {
     void ordonnancer();
 
     void changerVitesse(EnumVitesse vitesse);
 
-    void arreterOrdonnancement();
+    List<Agent> arreterOrdonnancement();
 }
--- src/main/java/com/m2dl/sma/infrastructure/ordonnanceur/StrategieClassique.java
+++ src/main/java/com/m2dl/sma/infrastructure/ordonnanceur/StrategieClassique.java
@@ -1,20 +1,57 @@
 package com.m2dl.sma.infrastructure.ordonnanceur;
 
 import com.m2dl.sma.infrastructure.EnumVitesse;
+import com.m2dl.sma.infrastructure.agent.Agent;
+import com.m2dl.sma.infrastructure.etat.IEtat;
+
+import java.util.List;
+import java.util.Optional;
 
 public class StrategieClassique implements IStratOrdonnanceur {
+
+    private List<Agent> listOrdonnancement;
+    private EnumVitesse vitesse;
+    private boolean run = true;
+    
     @Override
     public void ordonnancer() {
+        run = true;
+        Agent agentCourant;
+        while(run){
+            agentCourant = listOrdonnancement.get(0);
+            cycleDeVie(agentCourant);
+            listOrdonnancement.remove(agentCourant);
+            listOrdonnancement.add(agentCourant);
+        }
+    }
 
+    private void cycleDeVie(Agent agentCourant) {
+        IEtat etat = agentCourant.getEtatInitial();
+        executerEtat(etat);
+    }
+
+    private void executerEtat(IEtat etat) {
+        etat.executer().ifPresent(this::executerEtat);
     }
 
     @Override
     public void changerVitesse(EnumVitesse vitesse) {
-
+        this.vitesse = vitesse;
     }
 
     @Override
-    public void arreterOrdonnancement() {
+    public List<Agent> arreterOrdonnancement() {
+        run = false;
+        return listOrdonnancement;
+    }
 
+    @Override
+    public void agentAjoute(Agent agent) {
+        listOrdonnancement.add(agent);
+    }
+
+    @Override
+    public void agentRetire(Agent agent) {
+        listOrdonnancement.remove(agent);
     }
 }
