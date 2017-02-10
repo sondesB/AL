8fb34ba8061c1559bab2003896c6da3c7f9860c9
Thomas Bille <thomas.bille625@gmail.com>
2017-02-10 15:58:47 +0100

    Ajout tests ordonnanceur

---
+++ src/test/java/com/m2dl/sma/infrastructure/ordonnanceur/StrategieEtatAEtatTest.java
@@ -0,0 +1,27 @@
+package com.m2dl.sma.infrastructure.ordonnanceur;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class StrategieEtatAEtatTest {
+
+    @Test
+    public void ordonnancer() throws Exception {
+
+    }
+
+    @Test
+    public void updateAgent() throws Exception {
+
+    }
+
+    @Test
+    public void updateSuppr() throws Exception {
+
+    }
+
+}
