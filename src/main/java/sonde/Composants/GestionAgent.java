254ee74fa930b9c68b89753806508331f5d8a954
diamijaafar <diamijaafar@gmail.com>
2017-02-10 16:45:47 +0100

    Modified : GestionAgent | void serviceApparus

--- src/main/java/sonde/Composants/GestionAgent.java
+++ src/main/java/sonde/Composants/GestionAgent.java
@@ -1,31 +1,30 @@
 package sonde.Composants;
 
 import interfaceswcomp.OCService;
-import sonde.Services.Notification;
+import sonde.Services.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Jaafar on 03/02/2017.
  */
-public class GestionAgent implements Notification {
+public class GestionAgent implements Notification,DisparitionComposant {
 
-    //Liste des nouveaux services
-    Set<OCService> listServicesApparus;
-    //Liste des service disparus
-    Set<OCService> listServicesDisparus;
-    //map correspondance entre service et agent
-    Map<Integer, Integer> mapServiceAgent = new HashMap<>();
+    private ArrayList<Integer> RefAgent = new ArrayList<>(); //TypeListe : ReffAgent (class equipe infra)
 
     @Override
-    public void servicesApparus(Set<OCService> listServicesApparus) {
-        this.listServicesApparus = listServicesApparus;
+    public void servicesApparus(Set<OCService> listServicesApparus){
+        //plan
+        for(Iterator<OCService> iterator = listServicesApparus.iterator(); iterator.hasNext();){
+            OCService service = iterator.next();
+            RefferenceAgent RefAgent = createAgent(SetService(service)); //ReferenceAgent createAgent(IEtat) | IEtat SetService(OCService)
+            RefAgent.add(RegAgent);
+            addAgent(RefAgent,service); //addAgent(RefAgent,OCService
+        }
     }
 
     @Override
-    public void servicesDisparus(Set<OCService> listServicesDisparus) {
-        this.listServicesDisparus = listServicesDisparus;
+    public void servicesDisparus(Set<OCService> listServicesDisparus){
+
     }
 }
--- src/main/java/sonde/Services/DisparitionComposant.java
+++ src/main/java/sonde/Services/DisparitionComposant.java
@@ -4,4 +4,5 @@ package sonde.Services;
  * Created by Jaafar on 03/02/2017.
  */
 public interface DisparitionComposant {
+
 }
