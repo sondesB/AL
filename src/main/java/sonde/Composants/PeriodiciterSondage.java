f0cafb811a33a2864871a7f467437515999eb920
Rottana <rottanaly@ymail.com>
2017-02-10 15:16:21 +0100

    Periodicite Sondage

--- src/main/java/sonde/Composants/PeriodiciterSondage.java
+++ src/main/java/sonde/Composants/PeriodiciterSondage.java
@@ -1,6 +1,9 @@
 package sonde.Composants;
 
+import javax.security.auth.login.Configuration;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Jaafar on 03/02/2017.
@@ -10,5 +13,51 @@ public class PeriodiciterSondage extends JFrame {
     private JFrame frame;
     private JPanel panel;
     private JLabel labelPeriodicite;
+    private JTextField fieldPeriodicite;
+    private JButton btnOK;
+    private int periodicite = 0;
 
+    sonde.Services.Configuration conf;
+
+    public void createForm() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+
+        labelPeriodicite = new JLabel("Periodicite (ms)");
+        fieldPeriodicite = new JTextField(10);
+
+        btnOK = new JButton("OK");
+        btnOK.addActionListener(e -> { getPeriodiciteValue(); });
+
+        panel.add(labelPeriodicite);
+        panel.add(fieldPeriodicite);
+        panel.add(btnOK);
+
+        Dimension dimension = panel.getPreferredSize();
+        dimension.width = 200;
+        dimension.height = 100;
+        panel.setPreferredSize(dimension);
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void getPeriodiciteValue() {
+        String value = fieldPeriodicite.getText();
+        boolean parsable = true;
+        try {
+            periodicite = Integer.parseInt(fieldPeriodicite.getText());
+        } catch (NumberFormatException e) {
+            parsable = false;
+        }
+        if (!parsable) {
+            JOptionPane.showMessageDialog(frame, "Invalid input!");
+        } else {
+            frame.dispose();
+        }
+        conf.definirPeriodicite(periodicite);
+    }
 }
