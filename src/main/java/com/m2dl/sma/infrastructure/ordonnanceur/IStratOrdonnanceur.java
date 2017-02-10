6607ba33d2838ceb9ac653c643c9212335ecef9c
Thomas Bille <thomas.bille625@gmail.com>
2017-02-10 15:08:30 +0100

    Ajout methode changerVitesse et arreterOrdonnancement

--- src/main/java/com/m2dl/sma/infrastructure/ordonnanceur/IStratOrdonnanceur.java
+++ src/main/java/com/m2dl/sma/infrastructure/ordonnanceur/IStratOrdonnanceur.java
@@ -1,7 +1,12 @@
 package com.m2dl.sma.infrastructure.ordonnanceur;
 
+import com.m2dl.sma.infrastructure.EnumVitesse;
 import com.m2dl.sma.infrastructure.agent.Agent;
 
 public interface IStratOrdonnanceur {
     void ordonnancer();
+
+    void changerVitesse(EnumVitesse vitesse);
+
+    void arreterOrdonnancement();
 }
