3cbe2aef94eaf91058b61aa62bbcddb79c958577
xanous <aoun.anass@gmail.com>
2017-02-12 17:19:35 +0100

    Defintion of DisparitionComposant interface

--- src/main/java/sonde/Services/DisparitionComposant.java
+++ src/main/java/sonde/Services/DisparitionComposant.java
@@ -1,8 +1,11 @@
 package sonde.Services;
 
+import com.m2dl.sma.infrastructure.agent.ReferenceAgent;
+
 /**
  * Created by Jaafar on 03/02/2017.
  */
 public interface DisparitionComposant {
 
+    boolean verifierServiceDisparu(ReferenceAgent ref);
 }
