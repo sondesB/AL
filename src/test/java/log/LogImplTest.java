474d3644c4d666cbcfa3349c9e76c53b1f66c7b2
fredericDuarte <frederic.duarte@master-developpement-logiciel.fr>
2017-02-19 16:59:06 +0100

    Test 1er partie : lecture d'un fichier log

--- src/test/java/log/LogImplTest.java
+++ src/test/java/log/LogImplTest.java
@@ -13,6 +13,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Created by Blue on 16/02/2017.
@@ -34,12 +36,15 @@ public class LogImplTest extends TestCase {
     public void testLogApparitionComposant() throws Exception {
         ILogComposant log = new LogImpl();
         log.logApparitionComposant(component);
+        
+        fileInput("File.txt"); 
 
-        File file = new File("File.txt"); // LE path est mauvais, il faudrait un vrai dossier de stockage de log
+      /*
+       *   File file = new File("File.txt"); // LE path est mauvais, il faudrait un vrai dossier de stockage de log
         assertTrue(file.exists());
         assertTrue(file.length() > 0);
 
-        file.delete();
+        file.delete();*/
 
     }
 
@@ -79,6 +84,46 @@ public class LogImplTest extends TestCase {
         assertTrue(file.length() > 0);
 
         file.delete();
+    }
+    
+    @Test
+    public boolean fileInput(String nameFile)  {
+    	
+    	// display the path if correct
+        Path logPath = Paths.get("/main/java/log/file"); 
+    	System.out.format("%s%n", logPath.toUri());
+    	
+    	// create the file
+    	File file = new File(logPath + "/"+ nameFile);
+    	
+    	// exist or not, the file is created
+    	 if (file.createNewFile()){
+ 	        System.out.println("File is created!");
+ 	      }else{
+ 	        System.out.println("File already exists.");
+ 	      }
+
+    	// so read the file
+ 		FileInputStream in = null;
+
+ 		try {
+ 			in = new FileInputStream(file);
+           // diplay le the size
+ 			System.out.println(" size of bytes : "	+ in.available() );
+
+
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		} finally {
+ 			try {
+ 				if (in != null)
+ 					in.close();
+ 			} catch (IOException ex) {
+ 				ex.printStackTrace();
+ 			}
+ 		}
+ 	}
+    	
     }
 
 
