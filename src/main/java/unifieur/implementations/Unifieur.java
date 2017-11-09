3e74607c2d6c5c6281f1aff7429e706105bbe7bd
sondesB <sondes_94@hotmail.com>
2017-11-09 11:56:04 +0100

    Update Unifieur.java

--- src/main/java/unifieur/implementations/Unifieur.java
+++ src/main/java/unifieur/implementations/Unifieur.java
@@ -8,6 +8,14 @@ import unifieur.services.Matching;
  * permettant de savoir si deux OCService sont compatibles.
  */
 public class Unifieur implements Matching {
+
+    /**
+     * Match retourne si oui ou non deux OCService sont compatibles.
+     *
+     * @param serviceA OCService A
+     * @param serviceB OCService B
+     * @return boolean
+     */
     @Override
     public boolean match(OCService serviceA, OCService serviceB) {
         return serviceA.getServiceName().equals(serviceB.getServiceName());
